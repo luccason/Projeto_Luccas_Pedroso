594a6ee80649d967e00176ddc38e781bbd26c43c
Luccas On <luccas.assis@estudante.ufla.br>
2020-06-09 20:03:37 -0300

    Adicionado método MC3()

--- Classe C.php
+++ Classe C.php
@@ -15,10 +15,17 @@ class C {
             echo 'Método MC2 <br/>';
     }
   }
+  function MC3()
+    {
+        if (isset($this)) {
+            echo 'Método MC3 <br/>';
+    }
+  }
 }
 
 //Teste de impressão
 $c = new C);
 $c->MC1();
 $c->MC2();
+$c->MC3();
 ?>
