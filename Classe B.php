cbb7414461a0e7960f109cb4a5a252aa59730d48
Luccas On <luccas.assis@estudante.ufla.br>
2020-06-09 19:15:26 -0300

    Adicionada Classe B

---
+++ Classe B.php
@@ -0,0 +1,23 @@
+<?php
+class B {
+	public int $B1;
+	public float $B2;
+	
+    function MB1()
+    {
+        if (isset($this)) {
+            echo "Método MB1 <br/>";
+    }
+  }
+	function MB2()
+    {
+        if (isset($this)) {
+            echo 'Método MB2 <br/>';
+    }
+  }
+  
+//Teste de impressão
+$b = new B();
+$b->MB1();
+$b->MB2();
+?>
