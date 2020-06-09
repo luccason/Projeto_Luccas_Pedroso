aef74633e11bc284629720b508cc5cbb1ea1bcfb
Luccas On <luccas.assis@estudante.ufla.br>
2020-06-09 20:04:00 -0300

    Inserida a Classe D

---
+++ Classe D.php
@@ -0,0 +1,24 @@
+<?php
+class D {
+	public int $D1;
+	public string $D2;
+	
+    function MD1()
+    {
+        if (isset($this)) {
+            echo "Método MD1 Teste <br/>";
+    }
+  }
+	function MD2()
+    {
+        if (isset($this)) {
+            echo 'Método MD2 Teste <br/>';
+    }
+  }
+}
+
+//Teste de impressão
+$d = new D);
+$d->MD1();
+$d->MD2();
+?>
