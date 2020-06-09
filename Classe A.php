4313840877a13fba6c30b198ad1641bf20fc406c
Luccas On <luccas.assis@estudante.ufla.br>
2020-06-09 19:38:51 -0300

    Adicionada a Classe C e inserido novo método na Classe A

--- Classe A.php
+++ Classe A.php
@@ -15,10 +15,17 @@ class A {
             echo 'Método MA2 <br/>';
     }
   }
+	function MA3()
+    {
+        if (isset($this)) {
+            echo '”Alteração a classe A partir do clone” <br/>';
+    }
+  }
 }
 
 //Teste de impressão
 $a = new A();
 $a->MA1();
 $a->MA2();
+$a->MA3();
 ?>
---
+++ Classe C.php
@@ -0,0 +1,24 @@
+<?php
+class C {
+	public string $C1;
+	public float $C2;
+	
+    function MC1()
+    {
+        if (isset($this)) {
+            echo "Método MC1 <br/>";
+    }
+  }
+	function MC2()
+    {
+        if (isset($this)) {
+            echo 'Método MC2 <br/>';
+    }
+  }
+}
+
+//Teste de impressão
+$c = new C);
+$c->MC1();
+$c->MC2();
+?>
